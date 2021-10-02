779aed87f9037ea1e14fc4d1f88e644e02d09810
kimjinsoo <roorooro9933@gmail.com>
2021-10-02 12:20:08 +0900

    add user, city, travel repository

---
+++ src/main/java/com/nuitblanche/triple/domain/city/CityRepository.java
@@ -0,0 +1,6 @@
+package com.nuitblanche.triple.domain.city;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City,Long> {
+}
---
+++ src/main/java/com/nuitblanche/triple/domain/travel/TravelRepository.java
@@ -0,0 +1,6 @@
+package com.nuitblanche.triple.domain.travel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelRepository extends JpaRepository<Travel,Long> {
+}
